5ed992da9aa10cabbe9f6737f2a9aff860f228a2
rishikatamboli25@gmail.com <rishikatamboli25@gmail.com>
2026-08-22 14:34:32 +0530

    corrected order service design

--- order-service/models/Order.js
+++ order-service/models/Order.js
@@ -1,20 +1,56 @@
 import mongoose from 'mongoose';
 
 const orderSchema = new mongoose.Schema({
-  userId: { type: String, required: true }, // Ties the order to the user
+
+  userId: {
+    type: String,
+    required: true
+  },
+
   items: [{
-    productId: { type: String, required: true },
-    name: { type: String, required: true },
-    price: { type: Number, required: true },
-    quantity: { type: Number, default: 1 }
+    productId: {
+      type: String,
+      required: true
+    },
+
+    name: {
+      type: String,
+      required: true
+    },
+
+    price: {
+      type: Number,
+      required: true
+    },
+
+    quantity: {
+      type: Number,
+      default: 1
+    }
   }],
-  totalAmount: { type: Number, required: true },
-  status: { 
-    type: String, 
+
+  totalAmount: {
+    type: Number,
+    required: true
+  },
+
+  paymentMethod: {
+    type: String,
+    enum: ['COD', 'ONLINE'],
+    required: true
+  },
+
+  status: {
+    type: String,
     enum: ['Processing', 'Shipped', 'Delivered', 'Cancelled'],
     default: 'Processing'
   },
-  shippingAddress: { type: Object, required: true }
+
+  shippingAddress: {
+    type: Object,
+    required: true
+  }
+
 }, { timestamps: true });
 
 export default mongoose.model('Order', orderSchema);
